364dcfe9832524dad7439e970c47d67aac4b4317
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2024-04-07 16:14:36 -0500

    added re-planing to test code

--- src/navigation/global_planner/main_rrt_star.cpp
+++ src/navigation/global_planner/main_rrt_star.cpp
@@ -7,13 +7,22 @@ int main() {
 
   double stepSize = 0.5;
   double goalRadius = 1.0;
-  double minDistanceToObstacle = 0.2;
+  float minDistanceToObstacle = 0.2;
   int maxIterations = 100000000;
 
   double minX = -45, maxX = 45, minY = -30, maxY = 30;
 
   RRTStar rrt(stepSize, goalRadius, maxIterations, minX, maxX, minY, maxY, minDistanceToObstacle);
   rrt.readObstaclesFromFile("./GDC1.vectormap.txt");
+  rrt.generate(start, end);
+
+  // create obstacle from waypoint
+  Eigen::Vector2f waypoint(-25.323, 16.167);
+
+  // test re-planing
+  rrt.reset();
+  start = Point(-25.89, 16.156);
+  rrt.labelAsObstacle(waypoint);
   rrt.generate(start, end);
 
   return 0;
